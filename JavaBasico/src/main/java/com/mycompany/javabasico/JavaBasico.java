dee875dc097ada90edf0564fd2ea9166e20baae1
vitor <vitor@DESKTOP-ERJHPGO>
2025-04-26 00:15:25 -0300

    teste

---
+++ JavaBasico/src/main/java/com/mycompany/javabasico/JavaBasico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javabasico;
+
+/**
+ *
+ * @author vitor
+ */
+public class JavaBasico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
